8b6cabfb7c77316d033328077e8358ff4cd1efd4
Ravindra <ravi@cloudmunch.com>
2012-08-29 11:09:09 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+import javax.xml.parsers.*;
+import javax.xml.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+public class HelloWorld {
+	int id;
+    String name;
+
+	public static void main(String args[]) {
+		try {
+				System.out.println("Hello World --->");
+            }
+
+        catch (Exception e){
+										System.out.println("exception occurred ==>"+e);
+		  }
+	}
+}
